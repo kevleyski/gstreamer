ce180c1e1acc478eff135ee8ef9a90f8bda712bd
Kevin Staunton-Lambert <kevin.staunton-lambert@metacdn.com>
2021-06-22 14:33:49 +1000

    Move to meson

--- kjsl_build_macos.sh
+++ kjsl_build_macos.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# KJSL: macOS GStreamer builder
+
 HERE=$PWD
 
 export XML_CATALOG_FILES="/usr/local/etc/xml/catalog"
@@ -6,7 +9,7 @@ export PATH="/usr/local/opt/bison/bin:$PATH"
 export LDFLAGS="-L/usr/local/opt/bison/lib"
 
 deps () {
-  brew install glib gettext bison gobject-introspection  meson ninja pkg-config gnu-indent
+  brew install glib gettext bison gobject-introspection graphene libogg libvorbis opus orc theora pango meson ninja pkg-config gnu-indent gtk-doc xquartz
 }
 
 install_gstreamer () {
@@ -18,6 +21,11 @@ install_gstreamer () {
 
 install_plugins_base () {
   cd "$HERE/../gst-plugins-base" || exit 1
+  ./autogen.sh
+  ./configure
+  make -j $(nproc)
+  sudo make install
+
   meson build -Dglib:iconv=native -Dgst-plugins-base:examples=disabled -Dgst-plugins-bad:rtmp=disabled
   ninja -C build
   sudo ninja -C build install
